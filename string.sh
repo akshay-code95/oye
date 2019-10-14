9468a17b462592e9221bbcc30b851712e0fcb658
root <root@ip-172-31-1-249.us-east-2.compute.internal>
2019-10-14 14:21:37 +0000

    add

---
+++ string.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+string1="saurabh"
+string2="ankit"
+echo "$string1 $string2"
+string3="$string1  $string2"
+echo "$string3"
+
+string3+=" Authorized Training Centre"
+echo "$string3"
